d1c97498de727c20615ac94ad9ddf1e1071ff119
coddingp <theodore!seriakov@gmail.com>
2022-01-31 20:01:17 +0600

    Добавили логин, логаут и др.

--- core/urls.py
+++ core/urls.py
@@ -21,6 +21,9 @@ from profiles import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.movie_list, name='movie_list')
+    path('', views.movie_list, name='movie_list'),
+    path('me', views.my_profile, name='my_profile'),
+    path('login_page', views.login_page, name='login_page'),
+    path('logout_page', views.logout_page, name='logout_page')
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Movie
+from django.contrib.auth import authenticate, login, logout
 
 def movie_list(request):
 	movies = Movie.objects.all()
@@ -7,3 +8,25 @@ def movie_list(request):
 		'movies': movies
 	}
 	return render(request, 'profiles/movie_list.html', context)
+
+def my_profile(request):
+	profile = request.user.profile
+	context = {
+		'profile': profile
+	}
+	return render(request, 'profiles/profile.html', context)
+
+
+def login_page(request):
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return redirect('my_profile')
+	return render(request, 'profiles/login_page.html')
+
+def logout_page(request):
+	logout(user)
+	return redirect('movie_list')
