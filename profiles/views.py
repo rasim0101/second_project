416334ce9f1f00ca2def54cd605a9cd4dfca6da0
coddingp <theodore!seriakov@gmail.com>
2022-01-31 20:04:58 +0600

    добавили login_required

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Movie
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def movie_list(request):
 	movies = Movie.objects.all()
@@ -9,6 +10,7 @@ def movie_list(request):
 	}
 	return render(request, 'profiles/movie_list.html', context)
 
+@login_required(login_url='/login_page')
 def my_profile(request):
 	profile = request.user.profile
 	context = {
