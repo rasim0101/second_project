e33484eadcbdae82e83e320523cc736c1cea2b9f
coddingp <theodore!seriakov@gmail.com>
2022-02-26 17:19:24 +0600

    Оптимизировали запросы

--- blogs/views.py
+++ blogs/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from .models import Blog, Comment
 
 def blog_list(request):
-	blogs = Blog.objects.all()
+	blogs = Blog.objects.all().prefetch_related('comment_set')
 	context = {
 		'blogs': blogs
 	}
 	return render(request, 'blogs/blog_list.html', context)
 
 def comment_list(request):
-	comments = Comment.objects.all()
+	comments = Comment.objects.all().select_related('blog')
 	context = {
 		'comments': comments
 	}
